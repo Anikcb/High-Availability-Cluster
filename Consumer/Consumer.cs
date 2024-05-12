144f4f98027d2cc1fe2569178d336ad122d083a9
anikcb <anikchakraborty562@gmail.com>
2024-05-12 22:57:17 +0600

    First Step with basic cluster, not highly available yet

---
+++ Consumer/Consumer.cs
@@ -0,0 +1,31 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+var factory = new ConnectionFactory { Uri = new Uri("amqp://guest:guest@localhost:5672/") };
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+// channel.ExchangeDeclare(exchange: "logs", type: ExchangeType.Fanout);
+
+var queueName = channel.QueueDeclare("q.user.created");
+
+channel.QueueBind(queue: "q.user.created",
+                  exchange: "e.user.created",
+                  routingKey: "user.created");
+
+Console.WriteLine(" [*] Waiting for logs.");
+
+var consumer = new EventingBasicConsumer(channel);
+consumer.Received += (model, ea) =>
+{
+    byte[] body = ea.Body.ToArray();
+    var message = Encoding.UTF8.GetString(body);
+    Console.WriteLine($" [Received] {message}");
+};
+channel.BasicConsume(queue: "q.user.created",
+                     autoAck: true,
+                     consumer: consumer);
+
+Console.WriteLine(" Press [enter] to exit.");
+Console.ReadLine();
---
+++ Producer/Producer.cs
@@ -0,0 +1,23 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+
+var factory = new ConnectionFactory { Uri = new Uri("amqp://guest:guest@localhost:5672/") };
+
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+// channel.ExchangeDeclare(exchange: "e.user.created", type: ExchangeType.Topic);
+
+var routingKey = (args.Length > 0) ? args[0] : "user.created";
+
+for(int i=0; i<=10000; i++){
+    var message ="Hello World - " + i.ToString();
+    var body = Encoding.UTF8.GetBytes(message);
+    channel.BasicPublish(exchange: "e.user.created",
+                     routingKey: routingKey,
+                     basicProperties: null,
+                     body: body);
+    Console.WriteLine($" [x] Sent '{routingKey}':'{message}'");
+}
+
+
